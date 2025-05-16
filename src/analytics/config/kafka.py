23ab9f02c0fd552e25436e409e88354cbb0b2a1f
Caleb-Script <caleb-script@outlook.de>
2025-05-16 08:21:25 +0200

    Update kafka.py

--- src/analytics/config/kafka.py
+++ src/analytics/config/kafka.py
@@ -5,7 +5,7 @@ from analytics.config import env
 
 class KafkaSettings(BaseSettings):
     bootstrap_servers: str = "localhost:9092"
-    topic_log: str = "activity.log.analytics"
+    topic_log: str = "logstream.log.analytics"
     client_id: str = env.PROJECT_NAME
 
     class Config:
